02435f70f8b420fd982ce7a1c76228e81c772d97
PriyankarLavate2005 <priyankalavate909@gmail.com>
2024-12-13 06:16:07 +0530

    DLL

--- DoublyLL.py
+++ DoublyLL.py
@@ -19,13 +19,57 @@ class DoublyLL():
             print("Doubly Linked List is empty")
         else:
             n=self.head
-            while n.ref is not None:
-                 n=n.ref
+            while n.nref is not None:
+                  n=n.nref
             while n is not None:
                 print(n.data,end="")
                 n=n.pref
+    def insert_empty(self,data):
+        if self.head is None:
+            new_node=Node(data)
+            self.head=new_node
+        else:
+           print("linked List is Not Empty")
+    def add_begin(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+        else:
+            new_node.nref=self.head
+            self.head.pref=new_node
+            self.head=new_node
+    def add_end(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+        else:
+            n=self.head
+            while n.nref is not None:
+                n=n.nref
+            n.nref=new_node
+            new_node.nref=n
+    def add_after(self, data, x):
+        new_node = Node(data)
+        if self.head is None:
+            print("Linked List is empty")
+        else:
+            n = self.head
+            while n is not None:
+                if x == n.data:
+                   break
+                n = n.nref
+            if n is None:
+                print("Given node is not present")
+            else:
+                new_node.nref = n.nref
+                new_node.pref = n
+                if n.nref is not None:
+                    n.nref.pref = new_node
+                n.nref = new_node
 
 
 db=DoublyLL()
+db.add_begin(67)
+db.add_end(78)
+db.add_after(78,67)
 db.Print_LL()
-db.Print_rev()
